45fcbb65fb0e6972177b3a356a3f7b0af0b010be
Lilhorenikita <nikitapalnesto19@gmail.com>
2024-06-18 11:23:56 +0530

    feat:concourancy

--- Adminside/vite.config.js
+++ Adminside/vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createServer } from 'http';
 
-// https://vitejs.dev/config/
+// Define allowed origins
+const allowedOrigins = ['http://localhost:444', 'http://localhost:3000'];
+
+// Custom middleware to handle CORS
+function corsMiddleware(req, res, next) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  next();
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      overlay: "false"
+    },
+
+    headers: {
+      "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "1; mode=block",
+      "Content-Security-Policy": "upgrade-insecure-requests",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+
+    },
+    setupMiddlewares: (middlewares, { app }) => {
+      app.use(corsMiddleware); // Use the custom CORS middleware
+      return middlewares; // Return the middlewares array
+    },
+  }
+});
+
